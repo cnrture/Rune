afd138e4e33a2105c0eed618d18f33a433a2a315
GitHub Action <action@github.com>
2025-04-26 13:19:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/cnrture/getcontactandroidstudiopluginproject/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cnrture/getcontactandroidstudiopluginproject/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.cnrture.getcontactandroidstudiopluginproject.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cnrture.getcontactandroidstudiopluginproject.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
