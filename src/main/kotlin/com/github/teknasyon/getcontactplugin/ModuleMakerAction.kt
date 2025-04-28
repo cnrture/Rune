b541e00c98e74d4b2fa656d7bdecc418b47cd467
candroid <canerture@teknasyon.com>
2025-04-28 17:08:43 +0300

    Add ModuleMakerAction class for initiating module creation dialog

---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/ModuleMakerAction.kt
@@ -0,0 +1,20 @@
+package com.github.teknasyon.getcontactplugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+class ModuleMakerAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project: Project = event.project ?: return
+        val startingLocation: VirtualFile? = event.getData(CommonDataKeys.VIRTUAL_FILE)
+        val shouldUseStartingLocation = startingLocation != null && startingLocation.isDirectory
+
+        ModuleMakerDialogWrapper(
+            project = project,
+            startingLocation = if (shouldUseStartingLocation) startingLocation else null
+        ).show()
+    }
+}
