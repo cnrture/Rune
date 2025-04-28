fa7e3d7809ae93248070c237004de2b17a3aae1e
candroid <canerture@teknasyon.com>
2025-04-28 17:08:35 +0300

    Add MessageDialogWrapper class for displaying messages in a dialog

---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/MessageDialogWrapper.kt
@@ -0,0 +1,35 @@
+package com.github.teknasyon.getcontactplugin
+
+import com.github.teknasyon.getcontactplugin.common.Constants
+import com.intellij.openapi.ui.DialogWrapper
+import org.jetbrains.annotations.Nullable
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.Action
+import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JTextArea
+
+class MessageDialogWrapper(private val message: String) : DialogWrapper(true) {
+
+    init {
+        init()
+    }
+
+    @Nullable
+    override fun createCenterPanel(): JComponent {
+        val dialogPanel = JPanel(BorderLayout())
+        dialogPanel.preferredSize = Dimension(120, 100)
+
+        JTextArea(message).apply {
+            isEditable = false
+            dialogPanel.add(this, BorderLayout.CENTER)
+        }
+
+        return dialogPanel
+    }
+
+    override fun createActions(): Array<Action> {
+        return arrayOf(DialogWrapperExitAction("Okay", Constants.DEFAULT_EXIT_CODE))
+    }
+}
