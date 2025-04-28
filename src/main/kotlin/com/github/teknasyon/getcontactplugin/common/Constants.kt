f3b2aa2b51a30f130903458185d649e9521c6326
candroid <canerture@teknasyon.com>
2025-04-28 17:07:37 +0300

    Add Constants object for configuration values and FreeMarker version

---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/common/Constants.kt
@@ -0,0 +1,23 @@
+package com.github.teknasyon.getcontactplugin.common
+
+import freemarker.template.Configuration
+import freemarker.template.Version
+
+object Constants {
+    const val WINDOW_WIDTH = 840
+    const val WINDOW_HEIGHT = 800
+    const val FILE_TREE_WIDTH = 300
+    const val CONFIGURATION_PANEL_WIDTH = 540
+
+    const val ANDROID = "Android"
+    const val KOTLIN = "Kotlin / JVM"
+
+    const val DEFAULT_MODULE_NAME = ":repository:database (as an example)"
+    const val DEFAULT_SRC_VALUE = "EMPTY"
+
+    const val DEFAULT_BASE_PACKAGE_NAME = "app.source.getcontact"
+
+    const val DEFAULT_EXIT_CODE = 2
+
+    val FREEMARKER_VERSION: Version = Configuration.VERSION_2_3_30
+}
