ce6670f531486ea028b4d21e9996e3fba4ee73c7
candroid <canerture@teknasyon.com>
2025-05-02 23:31:46 +0300

    Add GetcontactButton and GetcontactToolWindowFactory for feature creation UI

---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/GetcontactToolWindowFactory.kt
@@ -0,0 +1,62 @@
+package com.github.teknasyon.getcontactplugin
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposePanel
+import androidx.compose.ui.unit.dp
+import com.github.teknasyon.getcontactplugin.components.GetcontactButton
+import com.github.teknasyon.getcontactplugin.theme.WidgetTheme
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+import javax.swing.JComponent
+
+class GetcontactToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val contentFactory = ContentFactory.getInstance()
+        val content = contentFactory.createContent(createToolWindowComponent(project), "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    private fun createToolWindowComponent(project: Project): JComponent {
+        return ComposePanel().apply {
+            setBounds(0, 0, 300, 300)
+            setContent {
+                WidgetTheme {
+                    GetcontactToolWindowContent(project)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun GetcontactToolWindowContent(project: Project) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                GetcontactButton(
+                    modifier = Modifier.weight(1f),
+                    text = "Create Module",
+                    onClick = { ModuleMakerDialogWrapper(project, null).apply { showAndGet() } },
+                )
+
+                GetcontactButton(
+                    modifier = Modifier.weight(1f),
+                    text = "Create Feature",
+                    onClick = { FeatureMakerDialogWrapper(project, null).apply { showAndGet() } }
+                )
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/components/GetcontactButton.kt
@@ -0,0 +1,30 @@
+package com.github.teknasyon.getcontactplugin.components
+
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun GetcontactButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primary,
+            contentColor = MaterialTheme.colors.onSurface,
+        )
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+        )
+    }
+}
