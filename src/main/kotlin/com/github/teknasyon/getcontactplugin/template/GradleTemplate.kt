7c0ddf057f65d07416b45511fcfcb9f4ab6a4957
candroid <canerture@teknasyon.com>
2025-05-03 16:47:55 +0300

    Add Gradle templates for Android and Kotlin modules

---
+++ src/main/kotlin/com/github/teknasyon/getcontactplugin/template/GradleTemplate.kt
@@ -0,0 +1,60 @@
+package com.github.teknasyon.getcontactplugin.template
+
+object GradleTemplate {
+    fun getAndroidModuleGradleTemplate(moduleName: String, dependencies: String) = """
+plugins {
+    id 'com.android.library'
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+}
+
+apply from: '../../gradle/config/flavorConfig.gradle'
+apply from: '../../gradle/config/keyhider/config.gradle'
+
+def configuration = rootProject.ext.configuration
+
+android {
+    namespace 'app.source.getcontact.$moduleName'
+    compileSdkVersion configuration.compileSdkVersion
+
+    defaultConfig {
+        minSdkVersion configuration.minSdkVersion
+        targetSdkVersion configuration.targetSdkVersion
+    }
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    with flavorConfig
+}
+
+dependencies {
+    $dependencies
+}""".trimIndent()
+
+    fun getKotlinModuleGradleTemplate() = """
+plugins {
+    id 'java-library'
+    id 'org.jetbrains.kotlin.jvm'
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(libs.ksp)
+    detektPlugins libs.detekt.formatting
+    detektPlugins libs.detekt.composeRules
+}""".trimIndent()
+}
