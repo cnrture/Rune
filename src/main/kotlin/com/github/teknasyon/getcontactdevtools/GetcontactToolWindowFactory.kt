a7fe97d7039faf2fbdb1ac800baec7d475c17a22
candroid <canerture@teknasyon.com>
2025-05-04 17:46:56 +0300

    Refactor GetcontactToolWindowFactory to include Tab and TabRow components for enhanced UI functionality

--- src/main/kotlin/com/github/teknasyon/getcontactdevtools/GetcontactToolWindowFactory.kt
+++ src/main/kotlin/com/github/teknasyon/getcontactdevtools/GetcontactToolWindowFactory.kt
@@ -2,7 +2,9 @@ package com.github.teknasyon.getcontactdevtools
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ViewModule
