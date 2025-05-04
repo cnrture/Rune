7387a17f238be53784a51ee728694bb5c9141b7b
candroid <canerture@teknasyon.com>
2025-05-04 20:51:55 +0300

    Refactor GetcontactCheckbox and GetcontactRadioButton for improved background handling and layout consistency

--- src/main/kotlin/com/github/teknasyon/getcontactdevtools/components/GetcontactCheckbox.kt
+++ src/main/kotlin/com/github/teknasyon/getcontactdevtools/components/GetcontactCheckbox.kt
@@ -1,24 +1,31 @@
 package com.github.teknasyon.getcontactdevtools.components
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.Checkbox
-import androidx.compose.material.CheckboxDefaults
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.teknasyon.getcontactdevtools.theme.GetcontactTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun GetcontactCheckbox(
     modifier: Modifier = Modifier,
     label: String,
     checked: Boolean,
+    isBackgroundEnable: Boolean = false,
     onCheckedChange: (Boolean) -> Unit = {},
 ) {
     Row(
@@ -28,21 +35,38 @@ fun GetcontactCheckbox(
                 role = Role.Checkbox,
                 onClick = { onCheckedChange(checked.not()) }
             )
-            .padding(end = 12.dp),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Checkbox(
-            checked = checked,
-            onCheckedChange = onCheckedChange,
-            colors = CheckboxDefaults.colors(
-                checkedColor = GetcontactTheme.colors.blue,
-                uncheckedColor = GetcontactTheme.colors.white,
+            .then(
+                if (isBackgroundEnable) {
+                    Modifier.background(
+                        color = GetcontactTheme.colors.orange,
+                        shape = RoundedCornerShape(12.dp),
+                    )
+                } else {
+                    Modifier
+                }
             )
-        )
+            .clip(RoundedCornerShape(12.dp))
+            .padding(12.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        CompositionLocalProvider(
+            LocalMinimumInteractiveComponentEnforcement provides false,
+        ) {
+            Checkbox(
+                checked = checked,
+                onCheckedChange = onCheckedChange,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.orange,
+                    uncheckedColor = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.white,
+                )
+            )
+        }
+        Spacer(modifier = Modifier.size(8.dp))
         Text(
             text = label,
-            color = GetcontactTheme.colors.white,
+            color = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.white,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
         )
     }
 }
--- src/main/kotlin/com/github/teknasyon/getcontactdevtools/components/GetcontactRadioButton.kt
+++ src/main/kotlin/com/github/teknasyon/getcontactdevtools/components/GetcontactRadioButton.kt
@@ -1,43 +1,71 @@
 package com.github.teknasyon.getcontactdevtools.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.teknasyon.getcontactdevtools.theme.GetcontactTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun GetcontactRadioButton(
     text: String,
     selected: Boolean,
+    isBackgroundEnable: Boolean = false,
     onClick: () -> Unit,
 ) {
     Row(
         modifier = Modifier
             .selectable(
                 selected = selected,
+                role = Role.RadioButton,
                 onClick = onClick,
             )
-            .padding(end = 16.dp),
+            .then(
+                if (isBackgroundEnable) {
+                    Modifier.background(
+                        color = GetcontactTheme.colors.orange,
+                        shape = RoundedCornerShape(12.dp),
+                    )
+                } else {
+                    Modifier
+                }
+            )
+            .clip(RoundedCornerShape(12.dp))
+            .padding(horizontal = 12.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        RadioButton(
-            colors = RadioButtonDefaults.colors(
-                selectedColor = GetcontactTheme.colors.blue,
-                unselectedColor = GetcontactTheme.colors.white,
-            ),
-            selected = selected,
-            onClick = onClick,
-        )
+        CompositionLocalProvider(
+            LocalMinimumInteractiveComponentEnforcement provides false,
+        ) {
+            RadioButton(
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.white,
+                    unselectedColor = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.white,
+                ),
+                selected = selected,
+                onClick = onClick,
+            )
+        }
+        Spacer(modifier = Modifier.size(8.dp))
         Text(
             text = text,
-            color = GetcontactTheme.colors.white,
+            color = if (isBackgroundEnable) GetcontactTheme.colors.gray else GetcontactTheme.colors.white,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
         )
     }
 }
