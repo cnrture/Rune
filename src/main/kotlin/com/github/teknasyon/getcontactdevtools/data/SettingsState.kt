112f772f195df3ec569a36d30b0cd8057ca01576
candroid <canerture@teknasyon.com>
2025-05-06 03:29:26 +0300

    Add SettingsService and SettingsState for managing application settings

---
+++ src/main/kotlin/com/github/teknasyon/getcontactdevtools/data/SettingsState.kt
@@ -0,0 +1,19 @@
+package com.github.teknasyon.getcontactdevtools.data
+
+import com.github.teknasyon.getcontactdevtools.common.Constants
+
+data class SettingsState(
+    var defaultPackageName: String = Constants.DEFAULT_BASE_PACKAGE_NAME,
+    var preferredModuleType: String = Constants.ANDROID,
+    var webViewUrl: String = Constants.DEFAULT_WEB_VIEW_URL,
+
+    var featureScreenTemplate: String = Constants.EMPTY,
+    var featureViewModelTemplate: String = Constants.EMPTY,
+    var featureContractTemplate: String = Constants.EMPTY,
+    var featureComponentKeyTemplate: String = Constants.EMPTY,
+    var featurePreviewProviderTemplate: String = Constants.EMPTY,
+    var moduleReadmeTemplate: String = Constants.EMPTY,
+    var manifestTemplate: String = Constants.EMPTY,
+    var gradleAndroidTemplate: String = Constants.EMPTY,
+    var gradleKotlinTemplate: String = Constants.EMPTY,
+)
