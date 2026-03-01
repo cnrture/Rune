810f2f170ca6587f30a4fda94c088b6cea62cdf0
candroid <cnrture@gmail.com>
2026-03-01 14:06:35 +0300

    initialize project structure with Gradle and IntelliJ plugin configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "Rune-Plugin"
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
+}
